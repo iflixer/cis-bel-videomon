f2ad09b053eb38e49934081f66890649234d6691
andyUA <grenway.andy@gmail.com>
2025-10-01 12:28:48 +0300

    More gap between tests

--- backend/cron-runner.js
+++ backend/cron-runner.js
@@ -61,7 +61,7 @@ async function runTest() {
 
     // Schedule next run dynamically
     const minutes = getTestDurationMinutes(config.test);
-    const intervalSec = (minutes * 60) + 20; // add 10s buffer
+    const intervalSec = (minutes * 60) + 60; // add 1min gap
     console.log(`Next run in ${intervalSec} sec...`);
 
     setTimeout(runTest, intervalSec * 1000);
